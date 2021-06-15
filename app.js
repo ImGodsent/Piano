1e81eaddd1df7dc9d5151d0e23d8fea3c03a2576
godsent <teamsushimaker@gmail.com>
2021-06-15 10:16:31 +0800

    add keydown function

--- app.js
+++ app.js
@@ -2,8 +2,21 @@ const keys = document.querySelectorAll(".key");
 const regularKeys = document.querySelectorAll(".white");
 const sharpKeys = document.querySelectorAll(".black");
 
+const whiteKeys = ["a", "s", "d", "f", "j" , "k" , "l"]; 
+const blackKeys = ["w", "e", "r", "u", "i"]; 
+
 keys.forEach((key) => {
   key.addEventListener("click", () => playNote(key));
+});
+
+document.addEventListener('keydown', (e) => {
+  if (e.repeat) return;
+  const key = e.key;
+  const whiteKeyIndex = whiteKeys.indexOf(key);
+  const blackKeyIndex = blackKeys.indexOf(key);
+
+  if (whiteKeyIndex > -1) playNote(regularKeys[whiteKeyIndex]);
+  if (blackKeyIndex > -1) playNote(sharpKeys[blackKeyIndex]);
 });
 
 let playNote = (key) => {
@@ -15,3 +28,4 @@ let playNote = (key) => {
     key.classList.remove("active");
   });
 };
+  
